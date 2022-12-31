6c031df904eadc5da79d94d258b3676b3d818959
kingaranjoroge <briankingara4@gmail.com>
2022-12-31 19:05:03 +0300

    first commit

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className="About">
+        <h2>About</h2>
+        <p style={{ marginTop: "1rem"}}>
+          This is an App for creating blogs 
+        </p>
+    </main>
+  )
+}
+
+export default About
